d2817d7c90d50fa10a54934c54d4ead49eaf8835
mevljas <sebastjan.mevlja@telekom.si>
2023-11-05 19:46:00 +0100

    improve data loading

--- assignments/assignment1/utils/file_manager.py
+++ assignments/assignment1/utils/file_manager.py
@@ -4,42 +4,60 @@ from typing import Tuple, Any
 
 class FileManager(object):
     @staticmethod
-    def find_cascade_files(base_path: str) -> [str]:
+    def find_cascade_filenames(base_path: str) -> [str]:
         """
         Reads the filenames from the given identities.txt file on base_path.
-        :param base_path: path to the directory whihc holds identities.txt file.
+        :param base_path: path to the directory which holds identities.txt file.
         :return: a list of cascade file paths.
         """
         lines = []
         with open(base_path+'identites.txt', "r") as file:
             # Reading from a file
             lines = file.readlines()
-        return [base_path+line.split(" ")[0] for line in lines]
+        return [base_path+'ears/'+(line.split(" ")[0].split(".png")[0]) for line in lines]
 
     @staticmethod
-    def prepare_data(base_path: str) -> tuple[[str], [str]]:
+    def read_ground_truths(filenames: [str]) -> {str}:
+        """
+        Reads the files containing grounds truths and saved them in a dictionary.
+        :param filenames: a list of files contain grounds truths.
+        :return: a dictionary of ground truths.
+        """
+        grounds_truths = {}
+        for filename in filenames:
+            with open(filename+'.txt', "r") as file:
+                # Reading from a file
+                line = file.readline()
+                _, x, y, w, h = line.split()
+                grounds_truths[filename] = (x, y, w, h)
+        return grounds_truths
+
+
+    @staticmethod
+    def prepare_data(base_path: str) -> tuple[[str], [str], [str], {str}]:
         """
         Prepares the data for training and testing.
         :param base_path: path to the directory whihc holds identities.txt file.
         :return: train and test sets with cascade file paths.
         """
-        cascade_filenames = FileManager.find_cascade_files(base_path=base_path)
-        train_set, test_set = FileManager.split_data_set(cascade_filenames=cascade_filenames, train_ratio=0.8)
+        filenames = FileManager.find_cascade_filenames(base_path=base_path)
+        train_set, test_set = FileManager.split_data_set(filenames=filenames, train_ratio=0.8)
+        ground_truths = FileManager.read_ground_truths(filenames=filenames)
 
-        return train_set, test_set
+        return filenames, train_set, test_set, ground_truths
 
     @staticmethod
-    def split_data_set(cascade_filenames: [str], train_ratio: float = 0.8):
+    def split_data_set(filenames: [str], train_ratio: float = 0.8):
         """
         Splits the given cascade file paths into train and test sets.
-        :param cascade_filenames: a list of cascade file paths.
+        :param filenames: a list of cascade file paths.
         :param train_ratio: percentage of the train set.
         :return: a tuple of train and test sets.
         """
-        train_size = int(len(cascade_filenames) * train_ratio)
+        train_size = int(len(filenames) * train_ratio)
 
-        train_set = cascade_filenames[0:train_size]
-        test_set = cascade_filenames[train_size:]
+        train_set = filenames[0:train_size]
+        test_set = filenames[train_size:]
 
         return train_set, test_set
 
