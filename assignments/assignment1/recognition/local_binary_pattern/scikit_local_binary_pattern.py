a45d56ff956edfc04c1530ba65541755a263ce0c
mevljas <sebastjan.mevlja@telekom.si>
2023-11-12 20:32:13 +0100

    code cleanup

--- assignments/assignment1/recognition/local_binary_pattern/scikit_local_binary_pattern.py
+++ assignments/assignment1/recognition/local_binary_pattern/scikit_local_binary_pattern.py
@@ -6,7 +6,7 @@ from skimage import io
 from skimage.color import rgb2gray
 
 
-class SkLocalBinaryPattern(object):
+class ScikitLocalBinaryPattern(object):
     @staticmethod
     def compute_lbp(image, radius, n_points):
         lbp = feature.local_binary_pattern(image, n_points, radius, method="uniform")
@@ -21,7 +21,7 @@ class SkLocalBinaryPattern(object):
         for radius in radii:
             for n in n_points:
                 for word_length in range(0, word_lengths + 1):
-                    lbp = SkLocalBinaryPattern.compute_lbp(gray_image, radius, n)
+                    lbp = ScikitLocalBinaryPattern.compute_lbp(gray_image, radius, n)
                     hist, _ = np.histogram(lbp, bins=np.arange(0, word_length + 1), density=True)
                     features.extend(hist)
 
@@ -31,7 +31,7 @@ class SkLocalBinaryPattern(object):
     def process_images(image_paths, radii, n_points, overlap, word_lengths):
         all_features = []
         for image_path in image_paths:
-            features = SkLocalBinaryPattern.extract_lbp_features(image_path, radii, n_points, overlap, word_lengths)
+            features = ScikitLocalBinaryPattern.extract_lbp_features(image_path, radii, n_points, overlap, word_lengths)
             all_features.append(features)
 
         return np.array(all_features)
@@ -58,7 +58,7 @@ class SkLocalBinaryPattern(object):
         overlap = 0.5  # Adjust overlap as needed
         word_lengths = 256  # Adjust word length as needed
 
-        features = SkLocalBinaryPattern.process_images(filenames, radii, n_points, overlap, word_lengths)
+        features = ScikitLocalBinaryPattern.process_images(filenames, radii, n_points, overlap, word_lengths)
 
         # Display the extracted features
         print("Extracted features:")
