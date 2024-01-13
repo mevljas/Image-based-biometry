fd6fe8dfae1b1524e87971f9fb5cb3919ef5e561
mevljas <sebastjan.mevlja@telekom.si>
2024-01-13 22:35:03 +0100

    update hog

--- assignments/assignment3/main.py
+++ assignments/assignment3/main.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
 
     # Initialize HOG feature extractor
     # Initialize HOG feature extractor with optimized parameters
-    hog_extractor = HOGFeatureExtractor(win_size=(256, 256), block_size=(16, 16), block_stride=(8, 8), cell_size=(8, 8),
+    hog_extractor = HOGFeatureExtractor(win_size=(64, 128), block_size=(16, 16), block_stride=(8, 8), cell_size=(8, 8),
                                         nbins=9)
 
     # Test HOG on ground truths
